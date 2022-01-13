be6ac5272d4ccaaf9c6d3d3efd2d36a9ee0d1ea1
tristan <jt.jeruta@gmail.com>
2022-01-14 00:33:35 +0800

    Add folder structure and templates

---
+++ src/components/sampleComponent.js
@@ -0,0 +1,7 @@
+const SampleComponent = () => {
+    return (
+        <div>This is a sample component</div>
+    )
+}
+
+export default SampleComponent;
---
+++ src/pages/sample.js
@@ -0,0 +1,15 @@
+import SampleComponent from "../components/sampleComponent";
+
+const SamplePage = () => {
+  return (
+    <div>
+        <h1>Sample Page</h1>
+        <br />
+        <SampleComponent />
+        <SampleComponent />
+        <SampleComponent />
+    </div>
+  );
+}
+
+export default SamplePage;
