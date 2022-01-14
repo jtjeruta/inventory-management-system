7f54e47cbf1b4979c99c322c2794acd1b259adcc
tristan <jt.jeruta@gmail.com>
2022-01-14 14:13:00 +0800

    Add styled components

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    color: palevioletred;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+    width: fit-content;
+    transition: transform 200ms;
+    cursor: default;
+
+    &:hover {
+        transform: translateY(-5px);
+    }
+`
 
 const SampleComponent = () => {
-    return <div>This is a sample component</div>
+    return <Container>This is a sample component</Container>
 }
 
 export default SampleComponent
